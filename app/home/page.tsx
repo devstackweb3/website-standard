bf0260e5a9a69cb92d3b715beb8d974239df23f7
AlexCarliner <testdevstack1@gmail.com>
2023-07-01 16:32:13 +0200

    Update N°1

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,14 @@
 'use client'
 import React from 'react'
+import IntroSection from './intro/page'
+import Footer from '@/app/components/Footer'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <IntroSection />
+      <Footer />
+    </div>
   )
 }
 
